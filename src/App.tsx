562449027bb818bc2ee4a017b6a6d66c55942342
Gary N. Bagnall <gbagnall8@gmail.com>
2021-01-20 00:08:28 +1000

    added phases to game

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 import styled from "styled-components";
 import Tower from "./components/Tower";
 
-import { Towers } from "./Types";
+import { Towers, TowerT } from "./Types";
+import { moveCheck, moveBlock } from "./hanoi";
 
 const Container = styled.div`
   position: absolute;
@@ -22,14 +23,27 @@ const TowersWrapper = styled.div`
   justify-content: space-evenly;
 `;
 
+const TOWER_LENGTH = 2;
+
 function App() {
   const [towers, setTowers] = useState<Towers>({
-    1: [1, 2, 3, 4, 5, 6],
+    1: Array.from({ length: TOWER_LENGTH }, (_, i) => i + 1),
     2: [],
     3: [],
   });
 
   const [selected, setSelected] = useState<string>("");
+
+  const isComplete = !towers[1].length && !towers[2].length;
+
+  const handlePhaseOne = (k: string, v: TowerT) => {
+    selected === k ? setSelected("") : (v.length || selected) && setSelected(k);
+  };
+
+  const handlePhaseTwo = (k: String) => {
+    moveCheck(selected, k, towers) && setTowers(moveBlock(selected, k)(towers));
+    setSelected("");
+  };
 
   useEffect(() => {
     console.log("selection changed");
@@ -41,21 +55,23 @@ function App() {
       <TowersWrapper>
         {Object.entries(towers).map(([k, v]) => (
           <Tower
-            key={k}
-            towers={towers}
-            akey={k}
-            onClick={() =>
-              selected === k
-                ? setSelected("")
-                : (v.length || selected) && setSelected(k)
+            tower={towers[k]}
+            color={
+              // If you dont want to use movecheck again in phase two, you can add the colours to the towers state object but then you need to remove them to process the move.
+              // or memoise movecheck
+              selected ? (moveCheck(selected, k, towers) ? "green" : "red") : ""
             }
-            selected={selected}
-            setSelected={setSelected}
-            setTowers={setTowers}
+            onClick={() =>
+              !selected ? handlePhaseOne(k, v) : handlePhaseTwo(k)
+            }
+            selected={selected === k}
           />
         ))}
       </TowersWrapper>
       <div style={{ fontSize: "5rem" }}>{selected}</div>
+      {isComplete && (
+        <div style={{ fontSize: "5rem", textAlign: "center" }}>You Win</div>
+      )}
     </Container>
   );
 }
--- src/components/Tower.tsx
+++ src/components/Tower.tsx
@@ -1,18 +1,14 @@
-import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
+import React from "react";
 import styled from "styled-components";
 import "./Tower.css";
 import Block from "./Block";
-import { Towers } from "../Types";
-import { moveCheck, moveBlock } from "../hanoi";
+import { TowerT } from "../Types";
 
 type Props = {
-  towers: Towers;
-  key: string;
-  akey: string;
+  tower: TowerT;
+  color: string;
   onClick: () => void;
-  selected: string;
-  setTowers: Dispatch<SetStateAction<Towers>>;
-  setSelected: Dispatch<SetStateAction<string>>;
+  selected: boolean;
 };
 
 const Wrapper = styled.div`
@@ -27,71 +23,17 @@ const Select = styled.div`
   background-color: lightblue;
 `;
 
-const Tower = ({
-  towers,
-  akey,
-  key,
-  onClick,
-  selected,
-  setSelected,
-  setTowers,
-}: Props) => {
-  const [color, setColor] = useState<string>("");
-  const [firstSelection, setfirstSelection] = useState<string>("");
-
-  useEffect(() => {
-    // Want to be able to unselect if clicking the same block.
-    // Dont do anything if you have clicked red.
-    // move it if you have something selected and the next selection is green
-
-    // If there is colour we are in phase 2
-    //TODO: checking if we are in phase two by checking if the blocks are coloured in is weird and the game phase should be stored in App
-
-    console.log("effect");
-    console.log(selected);
-
-    if (selected && !color) {
-      // Something has been selected and we are in game phase 1
-      setfirstSelection(selected);
-      moveCheck(selected, akey, towers) ? setColor("green") : setColor("red");
-    } else if (selected && color) {
-      // Something else has been selected and we are in game phase 2
-
-      // TODO: Change these to a switch statement, maybe useReducer
-      //only perform the action if we are working on displaying the selected tower (TODO: Move this logic else where)
-      if (selected === akey) {
-        switch (color) {
-          case "green":
-            setSelected("");
-            setTowers(moveBlock(firstSelection, selected)(towers));
-            break;
-          case "red":
-            setSelected(firstSelection);
-            break;
-          default:
-            break;
-        }
-      }
-      // is the NEW selection green? move the block
-    } else {
-      // No selection, reset
-      setSelected("");
-      setfirstSelection("");
-      setColor("");
-    }
-  }, [akey, color, firstSelection, selected, setSelected, setTowers, towers]);
-
+const Tower = ({ tower, onClick, selected, color }: Props) => {
   return (
     <Wrapper
-      key={key}
       onClick={onClick}
       style={{ backgroundColor: color }}
       className='rod'
     >
-      {towers[akey].map(disk => (
+      {tower.map(disk => (
         <Block key={disk} size={disk}></Block>
       ))}
-      {selected && akey === firstSelection && <Select />}
+      {selected && <Select />}
     </Wrapper>
   );
 };
